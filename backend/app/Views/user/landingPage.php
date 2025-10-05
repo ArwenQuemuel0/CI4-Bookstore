fdc915f64c26cf591a88b398dfe72087f96a3693
ArwenQuemuel0 <arwen.quemuel.030@gmail.com>
2025-10-06 01:01:57 +0800

    fixed icon not showing up

--- backend/app/Views/user/landingPage.php
+++ backend/app/Views/user/landingPage.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Achlys' Bookstore</title>
-    <link rel="shortcut icon" type="image/png" href="/assets/bookstore_icon.ico" />
+    <link rel="shortcut icon" type="image/png" href="/assets/book_icon.ico" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
